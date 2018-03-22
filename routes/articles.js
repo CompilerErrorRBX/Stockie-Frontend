451ddf22351fc8307e1f2d5f63aeff87475a887b
Stephen Martin <stephen.martin@cerner.com>
2018-03-21 21:43:56 -0500

    Add API endpoints

--- routes/articles.js
+++ routes/articles.js
@@ -32,14 +32,59 @@ const articleAPI = (app) => {
         // console.log(response);
         res.status(200);
         res.json(response.data);
+        next();
+      })
+      .catch((error) => {
+        console.log(error);
+        // res.status(500).send(error.response);
+      });
+  });
+
+  // Get an article by id
+  app.get('/article/:id/similar', (req, res, next) => {
+    axios.get(`${articleAPIURL}/article/${req.params.id}/similar`)
+      .then((response) => {
+        // console.log(response);
+        res.status(200);
+        res.json(response.data);
+        next();
+      })
+      .catch((error) => {
+        console.log(error);
+        // res.status(500).send(error.response);
+      });
+  });
+
+  // Get an article by id
+  app.get('/articles/search', (req, res, next) => {
+    const limit = req.query.limit ? parseInt(req.query.limit) : 10;
+    const offset = req.query.offset ? parseInt(req.query.offset) : 0;
+    const query = req.query.query;
+
+    axios.get(`${articleAPIURL}/articles/search?query=${query}&limit=${limit}&offset=${offset}`)
+      .then((response) => {
+        res.status(200);
+        res.json(response.data);
+        next();
       })
       .catch((error) => {
         console.log(error.response);
-        res.status(500).send(error.response.statusText);
+        // res.status(500).send(error.response);
       });
-
-    next();
   });
+
+  app.get('/articles/extract', (req, res, next) => {
+    axios.get(`${articleAPIURL}/article/extract`)
+      .then((response) => {
+        res.status(200);
+        res.json(response.data);
+        next();
+      })
+      .catch((error) => {
+        console.log(error.response);
+        // res.status(500).send(error.response);
+      });
+  });  
 }
 
 module.exports = articleAPI;
